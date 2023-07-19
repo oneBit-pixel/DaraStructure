9f51574ddd379c52295a632faa76fab6c37aa34c
948655206 <948655206@qq.com>
2023-07-20 00:10:43 +0800

    图的深度遍历

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -8,6 +8,9 @@ public class Graph {
     private ArrayList<String> vertexList;//存储顶点集合
     private int[][] edges;//存储图对应的邻结矩阵
     private int numOfEdges;
+    //定义给数组boolean[],记录某个结点是否被访问
+    private boolean[] isVisited;
+
 
     //构造器
     public static void main(String[] args) {
@@ -27,13 +30,71 @@ public class Graph {
         graph.insertEdge(1, 2, 1);//A-B
         graph.insertEdge(1, 3, 1);//A-B
         graph.insertEdge(1, 4, 1);//A-B
+
+        //显示一把邻结举证
+        graph.showGraph();
+
+        //测试一把，我们的dfs遍历是否ok
+        System.out.println("深度遍历");
+        graph.dfs();
     }
 
+    //构造器
     public Graph(int n) {
         //初始化矩阵和vertexList
         edges = new int[n][n];
         vertexList = new ArrayList<String>(n);
         numOfEdges = 0;
+        isVisited=new boolean[5];
+    }
+
+    //得到第一个领接结点的下标
+    public int getFirstNeighbor(int index) {
+        for (int j = 0; j < vertexList.size(); j++) {
+            if (edges[index][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    //根据前一个邻接结点下标来获取下一个邻接结点
+    public int getNextNeighbor(int v1, int v2) {
+        for (int j = v2 + 1; j < vertexList.size(); j++) {
+            if (edges[v1][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    //深度优先遍历算法
+    //i 第一次就是 0
+    public void dfs(boolean[] isVisited, int i) {
+        //首先访问该结点，输出
+        System.out.print(getValueByIndex(i) + "->");
+        //将结点设置为已经访问
+        isVisited[i] = true;
+        //查找结点i的第一个捆绑结点i
+        int w = getFirstNeighbor(i);
+        while (w != -1) {
+            //说明有
+            if (!isVisited[w]) {
+                dfs(isVisited, w);
+            }
+            //如果这个w结点已经被访问过
+            w = getNextNeighbor(i, w);
+        }
+    }
+
+    //对dfs 进行一个重载,遍历我们所有的结点并进行dfs
+    public void dfs() {
+        //遍历所有的结点，进行dfs[回溯]
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if (!isVisited[i]) {
+                dfs(isVisited, i);
+            }
+        }
     }
 
     //图中 常用的方法
